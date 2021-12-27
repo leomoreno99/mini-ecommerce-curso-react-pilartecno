59586a8040da488d0d5fbfd0500cd70aa5bfb701
Leonel Moreno <leonel_moreno99@hotmail.com>
2021-12-27 15:34:15 -0300

    changes in product card and header app

--- src/components/ListProducts.js
+++ src/components/ListProducts.js
@@ -7,7 +7,7 @@ export const ListProduct = () => {
     const products = json.products;
 
     return(
-        <Box sx={{ m: 16, mt:0, flexGrow: 1 }}>
+        <Box sx={{ m: "7%", mt:0, flexGrow: 1 }}>
             <Grid container spacing={2}>
                 {products.map((product, index) => (
                     <Product key={index} product={product}/>
--- src/components/Product.js
+++ src/components/Product.js
@@ -5,16 +5,30 @@ import {
   CardContent,
   CardMedia,
   Grid,
+  IconButton,
   Typography,
 } from "@mui/material";
 import { makeStyles } from "@mui/styles";
+import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 
 const useStyle = makeStyles({
-    cardHover: {
+  card: {
     "&:hover": {
       boxShadow: "0px 10px 10px rgb(0 0 0 / 20%);",
       cursor: "pointer",
-    }
+    },
+    borderRadius: "20px",
+  },
+
+  cardActions: {
+    justifyContent: "flex-end"
+  },
+
+  "@media (max-width: 600px)": {
+    backgroundCard: {
+      background:
+        "linear-gradient(0deg, rgba(255,255,255,1) 0%, rgba(255,255,255,0) 100%)",
+    },
   },
 });
 
@@ -22,15 +36,14 @@ export const Product = ({ product }) => {
   const { title, image, price, description } = product;
   const classes = useStyle();
 
-
   return (
-    <Grid item xs={6}>
-      <Card className={classes.cardHover} sx={{ maxWidth: "100%" }}>
+    <Grid item xs={12} md={6}>
+      <Card className={classes.card} sx={{ maxWidth: "100%" }}>
         <Grid container>
-          <Grid height={"15rem"} item xs={6}>
+          <Grid height={"15rem"} item xs={12} sm={6}>
             <CardMedia component="img" image={image} alt="green iguana" />
           </Grid>
-          <Grid item xs={6}>
+          <Grid item xs={12} sm={6} className={classes.backgroundCard}>
             <CardContent>
               <Typography
                 fontWeight="900"
@@ -54,7 +67,11 @@ export const Product = ({ product }) => {
                 ${price}
               </Typography>
             </CardContent>
-            <CardActions></CardActions>
+            <CardActions className={classes.cardActions} >
+              <IconButton color="primary" aria-label="add to shopping cart">
+                <AddShoppingCartIcon />
+              </IconButton>
+            </CardActions>
           </Grid>
         </Grid>
       </Card>
