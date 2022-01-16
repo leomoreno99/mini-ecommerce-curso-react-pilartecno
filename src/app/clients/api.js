f9c0f3d86a5be294d7e94987fc0a4e6e2a448df8
Leonel Moreno <leonel_moreno99@hotmail.com>
2022-01-16 19:28:57 -0300

    api.js refactoring

--- src/app/clients/api.js
+++ src/app/clients/api.js
@@ -1,27 +1,41 @@
-const get = async (url) => {
+const execute = async (url, config) => {
   try {
-    const res = await fetch(url);
+    const res = await fetch(url, config = {});
     return res.json();
   } catch (e) {
     throw new Error(e);
   }
+}
+
+const get = async (url) => {
+  return execute(url)
 };
 
 const post = async (url, body) => {
-  try {
-    const res = await fetch(url, {
-      method: "POST",
-      body: body,
-    });
-    return res.json();
-  } catch (e) {
-    throw new Error(e);
-  }
+  return execute(url, {
+    method: "POST",
+    body: body,
+  })
+};
+
+const put = async (url, body) => {
+ return execute (url, {
+  method: "PUT",
+  body: body,
+})
+};
+
+const delet = async (url) => {
+ return execute(url, {
+  method: "DELETE",
+})
 };
 
 const api = {
   get: (url) => get(url),
+  delete: (url) => delet(url),
   post: (url, body) => post(url, body),
+  put: (url, body) => put(url, body),
 };
 
 export default api;
